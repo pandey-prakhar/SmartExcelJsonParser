cbde914391005afd76f5db341e5f036dd213b541
pandey-prakhar <prakharpandey1198@gmail.com>
2025-03-08 00:39:42 +0530

    Prakhar: Initialize project with required dependencies for Excel-JSON conversion

---
+++ src/main/java/org/prakhar/smartexceljsonparser/SmartExcelJsonParserApplication.java
@@ -0,0 +1,13 @@
+package org.prakhar.smartexceljsonparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExcelJsonParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartExcelJsonParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/prakhar/smartexceljsonparser/SmartExcelJsonParserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prakhar.smartexceljsonparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExcelJsonParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
